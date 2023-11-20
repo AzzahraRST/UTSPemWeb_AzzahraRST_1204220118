91e64831b8845515c08820f32e0fe072d4667b8a
AzzahraRST <136779259+AzzahraRST@users.noreply.github.com>
2023-11-20 14:02:02 +0700

    UTSPemWeb_AzzahraRST_1204220118
    Azzahra Rizki Sulardi Trisnawati - 1204220118 - UTS Pemrograman Web - Mini Project Web

--- scripts.js
+++ scripts.js
@@ -1,15 +1,13 @@
-// JavaScript for scrolling to Content section
+
 function scrollToContent() {
   document.getElementById('content').scrollIntoView({ behavior: 'smooth' });
 }
 
-// JavaScript for navigating to specific links
 function navigateToLink(link) {
 
   console.log(`Navigating to ${link}`);
 }
 
-// JavaScript untuk hamburger menu toggle
 const menuToggle = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
 
